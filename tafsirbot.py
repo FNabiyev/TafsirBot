ade2bd14983bae7414cdf757be6e639b1acdea41
FNabiyev <nabiyevfazliddin17@gmail.com>
2021-08-25 09:50:57 +0500

    first commit

--- tafsirbot.py
+++ tafsirbot.py
@@ -4,11 +4,10 @@ from pprint import pprint
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 
-
 API_TOKEN = '1945582452:AAGvm9x_qM6b7uEiOcwS-JCzUGjyM2Yf_0s'
 
 # Configure logging
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
@@ -17,11 +16,13 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message, kb=None):
-    # print(message.from_user)
     fam = message.from_user.last_name
     ism = message.from_user.first_name
 
-    await message.reply("Assalomu Alaykum {} {}\nShu tartibda jo'nating:\n1:2 audio yoki 1:2 text\nko`rinishida jo`nating!\n\nBu yerda 1 birinchi sura 2 esa shu suradagi oyat\n\n#Eslatma\nAudio: Shayx Mishary Roshid\nTafsir: Shayx Alouddin Mansur".format(fam, ism))
+    await message.reply(
+        "Assalomu Alaykum {} {}\n\nShu tartibda jo'nating:\n1:2 audio yoki 1:2 text\nko`rinishida jo`nating!\n\nBu yerda 1 birinchi sura 2 esa shu suradagi oyat\n\n#Eslatma\nAudio: Shayx Mishary Roshid\nTafsir: Shayx Alouddin Mansur".format(
+            fam, ism))
+
 
 @dp.message_handler()
 async def send_audio(message: types.Message):
@@ -35,7 +36,8 @@ async def send_audio(message: types.Message):
             audio = r['data']['audio']
             await bot.send_audio(chat_id=message.from_user.id, audio=audio)
         elif key[1] == 'text':
-            url1 = 'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/uzb-alauddinmansour/{}/{}.json'.format(nw[0],nw[1])
+            url1 = 'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/uzb-alauddinmansour/{}/{}.json'.format(
+                nw[0], nw[1])
             url = 'http://api.alquran.cloud/v1/ayah/{}:{}/ar.alafasy'.format(nw[0], nw[1])
             res = requests.get(url)
             r = res.json()
@@ -44,10 +46,12 @@ async def send_audio(message: types.Message):
             text_arab = r['data']['text']
             text_uzb = r1['text']
 
-            await message.reply(text_arab+'\n\n'+text_uzb)
+            await message.reply(text_arab + '\n\n' + text_uzb)
     except:
-        await message.reply('No`to`g`ri formatda jo`natyapsiz!\nIltimos:\n\n1:2 audio yoki 1:2 text\nko`rinishida jo`nating!\n\nBu yerda 1 birinchi sura 2 esa shu suradagi oyat')
+        await message.reply(
+            'No`to`g`ri formatda jo`natyapsiz!\nIltimos:\n\n1:2 audio yoki 1:2 text\nko`rinishida jo`nating!\n\nBu yerda 1 birinchi sura 2 esa shu suradagi oyat')
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+    
